257f072dcc6b0d4776d08b47faea035f90dd71bc
Guilherme Henrique <103789281+Guihenrique62@users.noreply.github.com>
2023-05-17 20:51:29 -0300

    finish

--- src/modules/generateTextExample.js
+++ src/modules/generateTextExample.js
@@ -1,6 +1,4 @@
 export default function generateTextExample(){
    const textArea = document.getElementById('textInput')
-   if(textArea.value.length >= 0){
-      textArea.textContent ="Se você pressionar o botão de contagem, deverá ver a frequência absoluta dessas palavras em uma tabela e gráfico de pizza. Se você pressionar o botão de gerar exemplo, deverá ver esta mensagem. Considere isso um padrão"
-   }
+   textArea.value ="Se você pressionar o botão de contagem, deverá ver a frequência absoluta dessas palavras em uma tabela e gráfico de pizza. Se você pressionar o botão de gerar exemplo, deverá ver esta mensagem. Considere isso um padrão"
 }
